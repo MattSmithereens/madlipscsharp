a866ab30fe2bb1fa360a65b8d5fcdbece954a879
matt smith and Scott fraser <matt@>
2018-07-02 10:07:40 -0700

    build MVC framework

---
+++ mad-libs/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Madlibs.Models;
+
+namespace Madlibs.Controllers
+{
+    public class HomeController : Controller
+
+    {
+        [Route("/")]
+        public ActionResult Hello()
+        {
+            LetterVariable myLetterVariable = new LetterVariable();
+            myLetterVariable.SetRecipient("Lina");
+            myLetterVariable.SetSender("John");
+            return View(myLetterVariable);
+        }
+    }
+}
